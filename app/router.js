673be54cc4a32390987ca242a7e7d3841cfc9e1f
lijunliang <junguoliang@163.com>
2018-08-14 17:04:47 +0800

    新增接口路由

--- app/router.js
+++ app/router.js
@@ -33,6 +33,15 @@ module.exports = app => {
 
     // 获取用户信息
     router.get('/api_v2/currentUser', controller.user.currentUser);
+    // 获取用户列表
+    router.get('/api_v2/users', controller.user.index);
+    // 新增用户
+    router.post('/api_v2/users', controller.user.create);
+    // 修改用户信息
+    router.post('/api_v2/users/update', controller.user.update);
+    // 获取单个用户信息
+    router.get('/api_v2/users/info', controller.user.uinfo);
+
 
     // 站点列表
     router.get('/api_v2/station', controller.station.index);
@@ -49,4 +58,20 @@ module.exports = app => {
     // UPS信息列表
     router.get('/api_v2/ups_info', controller.upsinfo.index);
     router.post('/api_v2/ups_info', controller.upsinfo.update);
+
+    // 角色列表接口
+    router.get('/api_v2/roles', controller.roles.index);
+
+    // 获取bmsinfo列表
+    router.get('/api_v2/bmsinfo', controller.bmsinfo.index);
+    // 更新bmsinfo
+    router.post('/api_v2/bmsinfo', controller.bmsinfo.update);
+
+    // 报警设置相关
+    router.get('/api_v2/station_alert_desc', controller.stationAlertDesc.index); // 获取列表
+    router.post('/api_v2/station_alert_desc', controller.stationAlertDesc.update); // 更新报警设置
+    router.post('/api_v2/station_alert_desc/add', controller.stationAlertDesc.create); // 新增报警设置
+
+    // 参数列表
+    router.get('/api_v2/param', controller.param.index); // egg-socket 冲突所以使用这个名字，egg-socket中已经使用了paramter service
 };

af5918dd30a43c0fe7cecbcf58ab990511e5f7f4
lijunliang <junguoliang@163.com>
2018-08-14 17:04:25 +0800

    用户新增编辑列表接口

--- app/controller/user.js
+++ app/controller/user.js
@@ -52,6 +52,87 @@ class UserController extends Controller {
         };
     }
 
+    async index() {
+        const {ctx} = this;
+        let page=ctx.query.page || 1;
+
+        let data = await ctx.service.user.findAllByUser(ctx.session, 20*(page-1), 20);
+        console.log(data);
+        ctx.body = {
+            status: true,
+            data: {
+                list: data.rows,
+                totals: data.count,
+                page: page,
+                pageSize: 20
+            }
+        }
+    }
+
+    /**
+     * 新建用户
+     */
+    async create() {
+        const {ctx} = this;
+        let params = ctx.request.body;
+
+        try{
+            let result = await ctx.service.user.create(params);
+            ctx.body = {
+                status: true,
+                msg: '新建用户成功',
+            }
+        }catch(e){
+            ctx.body = {
+                status: false,
+                msg: `创建失败${e.message}`
+            }
+        }
+    }
+
+    /**
+     * 修改用户信息
+     */
+    async update() {
+        const {ctx} = this;
+        let params = ctx.request.body;
+
+        try{
+            let result = await ctx.service.user.update(params);
+            ctx.body = {
+                status: true,
+                msg: '修改用户成功',
+            }
+        }catch(e){
+            ctx.body = {
+                status: false,
+                msg: `修改用户信息失败${e.message}`
+            }
+        }
+    }
+
+    /**
+     * 获取单个用户信息
+     */
+    async uinfo(){
+        const {ctx} = this;
+        let uid = ctx.query.id;
+
+        try{
+            let data = await ctx.service.user.findUserById(uid);
+            ctx.body = {
+                status: true,
+                msg: '获取用户成功',
+                data: data
+            }
+        }catch(e){
+            ctx.body = {
+                status: false,
+                msg: `获取用户信息失败${e.message}`
+            }
+        }
+    }
+
 }
 
 module.exports = UserController;
